a449197d741e1a747bc79845a715495021bdf92c
Bhargav Ashok <bhargav.ashok2023@gmail.com>
2025-02-24 22:10:11 -0500

    Update app2.py

--- Iteration_2_SysEng_AI_Tool/app2.py
+++ Iteration_2_SysEng_AI_Tool/app2.py
@@ -5,10 +5,10 @@ from io import BytesIO
 import api_integration  # This module contains your integrated API, DB, and visualization functions
 
 # Set your API key as an environment variable so that the integration module can access it.
-os.environ["GOOGLE_API_KEY"] = "AIzaSyANC5TtyaWK7LS1ZiOOGIzZKX4rxHWrJaA"  # Replace with your actual API key
+os.environ["GOOGLE_API_KEY"] = "X"  # Replace with your actual API key
 
 app = Flask(__name__)
-app.secret_key = "VT202527"  # Replace with your own secret key
+app.secret_key = "X"  # Replace with your own secret key
 
 # Automatically load the PDF training data at startup.
 pdf_path = "C:\\Users\\bharg\\Downloads\\Wach_PF_D_2023_main.pdf"
